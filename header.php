3cd9e986bec8a379f650c6a59f5fe922fb2a8191
Gary McNally <garymcnally@gmail.com>
2022-11-16 13:28:10 +0000

    adding hotjar

--- header.php
+++ header.php
@@ -36,6 +36,18 @@
 </script>
 <!--End Analytics-->
 
+<!-- Hotjar Tracking Code for https://anchorpointinteriors.com/ -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3242168,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
 </head>
 
 <body <?php body_class(); ?>>
